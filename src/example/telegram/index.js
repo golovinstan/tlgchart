17d57b59876438b5c95a72317f7439e66261a0e2
golovin.stan@gmail.com <golovin.stan@gmail.com>
2019-03-27 14:28:58 +0300

    Chart caption

--- src/example/telegram/index.js
+++ src/example/telegram/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import TestData from './testdata';
 import Chart from './chart';
+import ChartCaption from './chartcaption';
+import { COLOR_DEFAULT } from '../../components/misc/color';
 
 
 class TelegramContest extends Component {
     render() {
         return (
             <div>
+                <ChartCaption
+                    width={"100%"}
+                    height={50}
+                    caption={`https://github.com/golovinstan/tlgchart`}
+                    color={COLOR_DEFAULT}
+                />                
                 {
                     TestData.map( (data, i) => <Chart data={data} caption={`Telegram contest data № ${i+1}`} key={i} /> )
                 }
