ff3aeb3096497138e2ec6a13ba21834586182273
golovin.stan@gmail.com <golovin.stan@gmail.com>
2019-04-14 03:26:31 +0300

    Stage 2 data prepare

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import * as serviceWorker from './serviceWorker';
 // import TwoAxisChart from './example/twoaxischart';
 // ReactDOM.render(<TwoAxisChart />, document.getElementById('root'));
 
-import TelegramContest from './example/telegram';
-ReactDOM.render(<TelegramContest />, document.getElementById('root'));
+//import TelegramContest from './example/telegram';
+//ReactDOM.render(<TelegramContest />, document.getElementById('root'));
+
+import TelegramContestStage2 from './example/stage2';
+ReactDOM.render(<TelegramContestStage2 />, document.getElementById('root'));
 
 // import DrawMousePath from './test/drawmousepath';
 // ReactDOM.render(<DrawMousePath />, document.getElementById('root'));
